7eaf3b5b171010d3775bc13bf239ecedf21cafa9
Trebor <23312931+RAMulc@users.noreply.github.com>
2020-09-08 21:56:23 +0800

    script.js refactored

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,59 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Variable for password length
+var passwordLength = 0;
+
+// Combined string of characters for password generation
+var selectedChars = "";
+
+
+function getPasswordLength() {
+  // Ask user for desired number of characters in password, validate the input
+  while (true) {
+    passwordLength = prompt("What length of password do you require? \n8 character minimum, 128 character maximum");
+    //Abort if null
+    if (passwordLength == null) {
+      return false;
+    }
+    //Check for valid password length, exit function if valid
+    if (parseInt(passwordLength) >= 8 && parseInt(passwordLength) <= 128) {
+      return true;
+    }
+  }
+}
+
+function getPasswordCharSet(allCharacters) {
+  // Password options stored as array, all set true initially
+  // lowercase, uppercase, numeric, special
+  var options = [true, true, true, true];
+  if (allCharacters.length !== options.length) {
+    return false;
+  }
+  // Ask user for password character criteria, store in array for future use, check at least one character set is selected
+  var isValid = false;
+  while (!isValid) {
+    options[0] = confirm("Include lowercase characters in the password?");
+    options[1] = confirm("Include uppercase characters in the password?");
+    options[2] = confirm("Include numeric characters in the password?");
+    options[3] = confirm("Include special characters in the password?");
+    if (options.indexOf(true) !== -1) {
+      console.log("Password criteria: (lowercase, uppercase, numeric, special)", options);
+      isValid = true;
+    }
+    else {
+      alert("Please select at least one character set!");
+    }
+  }
+  // Generate combined string of charcters from user selection
+  for (var i = 0; i < options.length; i++) {
+    if (options[i]) {
+      selectedChars += allCharacters[i];
+    }
+  }
+  return true;
+}
+
 function generatePassword() {
   var alphaLower = "abcdefghijklmnopqrstuvwxyz";
   var alphaUpper = alphaLower.toUpperCase();
@@ -10,42 +63,21 @@ function generatePassword() {
   var allChar = [alphaLower, alphaUpper, numeric, specChar];
   console.log("All Characters: ", allChar);
 
-  // Ask user for desired number of characters in password, validate the input
-  var validInput = false;
-  while (!validInput) {
-    var passwordLength = prompt("What length of password do you require? \n(8 character minimum, 128 character maximum");
-    if (passwordLength !== null && parseInt(passwordLength) >= 8 && parseInt(passwordLength) <= 128) {
-      validInput = true;
+  if (getPasswordLength() && getPasswordCharSet(allChar)) {
+    console.log("getPasswordLength: ", passwordLength);
+    console.log("Selected character list: ", selectedChars);
+
+    // Generate password
+    var generatedPassword = "";
+    for (var j = 0; j < passwordLength; j++) {
+      generatedPassword += selectedChars[getRandom(selectedChars.length)];
     }
+    console.log("Generated Password: ", generatedPassword, "Password Length: ", generatedPassword.length);
+    return generatedPassword;
   }
-  console.log("Password length: ", passwordLength);
-
-  // Password options stored as array, all set true initially
-  // lowercase, uppercase, numeric, special
-  var options = [true, true, true, true];
-  // Ask user for password character criteria, store in array for future use
-  options[0] = confirm("Include lowercase characters in the password?");
-  options[1] = confirm("Include uppercase characters in the password?");
-  options[2] = confirm("Include numeric characters in the password?");
-  options[3] = confirm("Include special characters in the password?");
-  console.log("Password criteria: (lowercase, uppercase, numeric, special)", options);
-
-  // Generate combined array of charcters from user selection
-  var selectedChar = "";
-  for (var i = 0; i < options.length; i++) {
-    if (options[i]) {
-      selectedChar += allChar[i];
-    }
-  }
-  console.log("Selected character list: ", selectedChar);
-
-  // Generate password
-  var generatedPassword = "";
-  for (var j = 0; j < passwordLength; j++) {
-    generatedPassword += selectedChar[getRandom(selectedChar.length)];
-  }
-  console.log("Generated Password: ", generatedPassword, "Password Length: ", generatedPassword.length);
-  return generatedPassword;
+  //User has aborted, end gracefully
+  alert("Something went wrong.")
+  return "";
 
 }
 
@@ -54,20 +86,17 @@ function getRandom(upperLimit) {
   return Math.floor(Math.random() * upperLimit);
 }
 
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if (password !== ""){
+    passwordText.value = password;
+  }
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Just a test - delete me
-for (var x = 0; x < 10; x++) {
-  console.log(getRandom(6));
-}
